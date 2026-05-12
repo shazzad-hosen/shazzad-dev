e55fe705299869ede706788dbcf3f03c4d655393
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-12 13:11:25 +0600

    basic frontend setup

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import demo from "./demo.png";
+import profile_image from "profile_image.jpg";
+
+const assets = {
+  logo,
+  demo,
+  profile_image,
+};
+
+export default assets;
